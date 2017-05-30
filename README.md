e29d54b74048123c839697517d4b579561b97d43
Daniel Pfeiffer <dpfeiffer@gowithfloat.com>
2017-05-30 08:50:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+adapt-question-feedback
+=================
+The **Question Feedback Component** provides a component for displaying feedback for a question. Instead of displaying feedback in an alert view, the feedback is displayed right on the page and conforms to the styling of all the other components.
+
+The component will display the body text until the question is answered. Then the body text will be replaced with the feedback specified in the question. Additionally, if there is item-level feedback provided, that will also be displayed.
+
+Consider using with the [Trickle](https://github.com/adaptlearning/adapt-contrib-trickle) extension to hide the feedback component until the question is answered.
+
+Don't forget to disable [Tutor](https://github.com/adaptlearning/adapt-contrib-tutor) and the [Detailed Feedback](https://github.com/gowithfloat/adapt-detailed-feedback) extensions!
+
+Settings
+--------
+### Attributes
+**_component (string)**: This value must be: `question-feedback`.
+
+> **displayTitle (string)**: The title to display above the feedback.
+
+> **body (string, optional)**: A message to display before feedback is shown. When the question is answered, this text will be replaced with feedback for the question.
+
+> **_classes (string)**: CSS class name to be applied to the button's containing div. The class must be predefined in one of the Less files. Separate multiple classes with a space.
+
+> **_layout (string)**: This defines the horizontal position of the component in the block. Acceptable values are full, left or right.
+
+> **_questionId (string, optional)**: Manually specify the ID of the question for which to show feedback. If empty, the component will try to find the question automatically by first looking in the same block, then looking for the immediately preceding question.
+
+> **_hideSelectedItem (boolean, default=false)**: Enable to hide the labels for the selected items when showing the selection-specific feedback.
+
+Limitations
+-----------
+No known limitations.
+
+-----------
+The **Question Feedback Component** is a plugin for the Adapt Framework. [Adapt](https://www.adaptlearning.org) is a free and easy to use e-learning authoring tool that creates fully responsive, multi-device, HTML5 e-learning content using the award-winning Adapt developer framework.
---
+++ js/adapt-question-feedback.js
@@ -0,0 +1,100 @@
+define(["coreJS/adapt", "coreViews/componentView"], function(Adapt, ComponentView) {
+    var FeedbackView = ComponentView.extend({
+        initialize: function() {
+            ComponentView.prototype.initialize.apply(this, arguments);
+
+            Adapt.on("questionView:showFeedback", this.onQuestionFeedbackRequested, this);
+            Adapt.on("notify:opened", this._handleNotify, this);
+            this.model.on("change:feedback", this.render, this);
+        },
+
+        preRender: function() {
+            this._findAssociatedQuestion();
+            this.model.set("_question", Adapt.findById(this.model.get("_questionId")));
+        },
+
+        postRender: function() {
+            this.setReadyStatus();
+
+            // Remove the "Show Feedback" button on the associated question
+            var questionId = this.model.get("_questionId");
+
+            if (!questionId) {
+                return;
+            }
+
+            var buttons = $(".buttons", "." + questionId);
+
+            $(".buttons-feedback", buttons).remove();
+            $(".buttons-marking-icon", buttons).remove();
+            $(".buttons-action", buttons).css("width", "100%");
+        },
+
+        preRemove: function() {
+            Adapt.off("notify:opened", this._handleNotify);
+        },
+
+        onQuestionFeedbackRequested: function(questionView) {
+            if (questionView.model.get("_id") !== this.model.get("_questionId")) {
+                return;
+            }
+
+            this.displayFeedback(questionView.model);
+        },
+
+        displayFeedback: function(questionModel) {
+            questionModel.setupFeedback();
+
+            this.model.set("isCorrect", questionModel.get("_isCorrect"));
+            this.model.set("selectedItems", questionModel.get("_selectedItems"));
+            this.model.set("feedback", questionModel.get("feedbackMessage"));
+
+            if (this.model.get("_isVisible")) {
+                this.setCompletionStatus();
+            } else {
+                this.model.on("change:_isVisible", this.setCompletionStatus, this);
+            }
+        },
+
+        _handleNotify: function(notify) {
+            var question = this.model.get("_question");
+            if (notify.model.get("title") === question.get("title")) {
+                notify.disableAnimation = true;
+                notify.remove();
+                setTimeout(function() {
+                    notify.closeNotify();
+                }, 1);
+            }
+        },
+
+        _findAssociatedQuestion: function() {
+            if (this.model.get("_questionId") && this.model.get("_questionId").length) {
+                return;
+            }
+
+            // If a question wasn't manually specified, see if there is a question in the same block
+            var question = this.model.getSiblings().findWhere({"_canShowFeedback": true});
+
+            if (!question) {
+                // If there isn't a question in the same block, find the immediately preceding question
+                var components = this.model.findAncestor().findDescendants("components");
+                components.reset(components.first(components.indexOf(this.model)));
+                var questions = components.where({"_canShowFeedback": true});
+
+                if (questions.length > 0) {
+                    question = questions[questions.length - 1];    
+                }
+            }
+            
+            if (!question) {
+                return;
+            }
+
+            this.model.set("_questionId", question.get("_id"));
+        }
+    });
+
+    Adapt.register("question-feedback", FeedbackView);
+
+    return FeedbackView;
+});
